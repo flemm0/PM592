74baa0a740d2decb208a9adb0aef0aee3aef9479
Flemming Wu <flemming.wu@gmail.com>
2023-10-07 16:42:23 -0700

    Saving HW5 and adding practice exam stuff

--- Homeworks/HW05/hw5_code.R
+++ Homeworks/HW05/hw5_code.R
@@ -27,26 +27,34 @@ lm(satisfac ~ classtype.f + rpe + encourage + control + perc_comp + bmi + age, d
 beta_pct_change <- function(adj, unadj) {
   return((abs((unadj - adj)/unadj))*100)
 }
-beta_pct_change(4.1245, 4.17838) # strength
-beta_pct_change(.9213, 1.03237) # flexibility
-beta_pct_change(.2610, .27563) # perceived exertion
-beta_pct_change(.5246, .46771) # encouragement
-beta_pct_change(.3947, .38527) # control
+beta_pct_change(4.1245, 4.17708) # strength
+beta_pct_change(.9213, 1.03705) # flexibility
+beta_pct_change(.2610, .27592) # perceived exertion
+beta_pct_change(.5246, .47857) # encouragement
+beta_pct_change(.3947, .43744) # control
 beta_pct_change(.4158, .42932) # competence
 
 
 ## Question 2
 
 # 2a
-lm(satisfac ~ (rpe + encourage + control + perc_comp)*classtype.f + age + bmi, data=gx) %>% summary()
+lm(satisfac ~ rpe*classtype.f + encourage + control + perc_comp, data=gx) %>% summary()
+lm(satisfac ~ rpe + encourage*classtype.f + control + perc_comp, data=gx) %>% summary()
+lm(satisfac ~ rpe + encourage + control*classtype.f + perc_comp, data=gx) %>% summary()
+lm(satisfac ~ rpe + encourage + control + perc_comp*classtype.f, data=gx) %>% summary()
+
 
 # 2b
-m <- lm(satisfac ~ age + bmi + rpe*classtype.f + encourage + control + perc_comp, data=gx) 
+m <- lm(satisfac ~ classtype.f*(rpe + encourage) + control + perc_comp, data=gx) 
 summary(m)
 
 # 2c
 interact_plot(m, pred = rpe, modx = classtype.f)
 sim_slopes(m, pred = rpe, modx = classtype.f)
+
+interact_plot(m, pred = encourage, modx = classtype.f)
+sim_slopes(m, pred = encourage, modx = classtype.f)
+
 
 # 2d
 autoplot(m)
