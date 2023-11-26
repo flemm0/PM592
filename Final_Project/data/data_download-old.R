7e10497bcee84ca2503020c0fd56751301f6a2e4
Flemming <flemming.wu@gmail.com>
2023-11-26 12:59:03 -0800

    create dataset for final project analysis

--- Final_Project/data/data_download-old.R
+++ Final_Project/data/data_download-old.R
@@ -1,93 +1,9 @@
 library(haven)
 library(tidyverse)
-library(skimr)
-
-# outcome variable: fasting glucose levels
-# independent variables: physical activity, how healthy is diet
-# confounders: age, bmi
-# interaction terms: drink alcohol, gender
-
-glu <- haven::read_xpt("Final_Project/data/P_GLU.XPT", 
-                       col_select = c("SEQN", "LBXGLU")) %>% 
-  data.frame() %>%
-  rename("glucose" = "LBXGLU")
-
-demographics <- haven::read_xpt("Final_Project/data/P_DEMO.XPT", 
-                                col_select = c("SEQN", "RIAGENDR", "RIDAGEYR")) %>%
-  data.frame() %>%
-  rename("age" = "RIDAGEYR", "gender" = "RIAGENDR")
-
-bmi <- haven::read_xpt("Final_Project/data/P_BMX.XPT", 
-                       col_select = c("SEQN", "BMXBMI")) %>%
-  data.frame() %>%
-  rename("bmi" = "BMXBMI")
-
-alcohol <- haven::read_xpt("Final_Project/data/P_ALQ.XPT", 
-                           col_select = c("SEQN", "ALQ121")) %>%
-  data.frame() %>%
-  rename("alcohol_frequency" = "ALQ121")
-
-pa <- haven::read_xpt("Final_Project/data/P_PAQ.XPT",
-                      col_select = c("SEQN",
-                                     "PAQ610", # days vigorous work
-                                     "PAD615", # avg mins vigorous work
-                                     "PAQ625", # days moderate work
-                                     "PAD630", # avg mins moderate work
-                                     "PAQ655", # days vigorous recr activity
-                                     "PAD660", # avg mins vigorous recr activity
-                                     "PAQ670", # days moderate recr activity
-                                     "PAD675" # avg mins moderate recr activity
-                                     )) %>%
-  data.frame() %>%
-  rename(
-    "days_vigorous_work" = "PAQ610", 
-    "avg_mins_vigorous_work" = "PAD615", 
-    "days_moderate_work" = "PAQ625",
-    "avg_mins_moderate_work" = "PAD630",
-    "days_vigorous_recr_activity" = "PAQ655",
-    "avg_mins_vigorous_recr_activity" = "PAD660",
-    "days_moderate_recr_activity" = "PAQ670",
-      "avg_mins_moderate_recr_activity" = "PAD675" 
-    )
-
-diet <- haven::read_xpt("Final_Project/data/P_DBQ.XPT", 
-                        col_select = c("SEQN", "DBQ700")) %>%
-  data.frame() %>%
-  rename("diet_health" = "DBQ700")
-
-
-nhanes <- 
-  glu %>%
-  inner_join(demographics) %>%
-  inner_join(bmi) %>%
-  inner_join(alcohol) %>%
-  inner_join(pa) %>%
-  inner_join(diet) %>%
-  tibble()
-
-
-head(nhanes)
-
-skimr::skim(nhanes)
-
-# fill NA in physical activity with 0
-pa_cols <- grep("^days_|^avg_mins_", names(nhanes), value = TRUE)
-nhanes[pa_cols] <- lapply(nhanes[pa_cols], function(x) ifelse(is.na(x), 0, x))
-
-
-# remove "missing" or "don't know" values in physical activity columns  
-nhanes <- 
-  nhanes %>%
-  filter(across(starts_with("days_"), ~ . <= 7)) %>%
-  filter(across(starts_with("avg_mins_"), ~ . <= 900)) %>% skim()
-
-skim(nhanes)
-
-
 
 ## read in data
-glu <- haven::read_xpt("~/USC_Fall_2023/PM592/Final_Project/data/P_GLU.XPT", 
-                       col_select = c("SEQN", 
+glu <- haven::read_xpt("~/USC_Fall_2023/PM592/Final_Project/data/P_GLU.XPT",
+                       col_select = c("SEQN",
                                       "LBXGLU" # Fasting Glucose (mg/dL)
                                       ))
 demo <- haven::read_xpt("~/USC_Fall_2023/PM592/Final_Project/data/P_DEMO.XPT",
@@ -104,7 +20,7 @@ dbq <- haven::read_xpt("~/USC_Fall_2023/PM592/Final_Project/data/P_DBQ.XPT",
                                       "SEQN",
                                       "DBD895", # meals not home prepared
                                       "DBD910" # no. of frozen meals/pizza in past 30 days
-                                      )) 
+                                      ))
 fsq <- haven::read_xpt("~/USC_Fall_2023/PM592/Final_Project/data/P_FSQ.XPT",
                        col_select = c("SEQN",
                                       "FSDHH", # household food security category
@@ -148,7 +64,7 @@ skim(nhanes)
 nhanes <-
   nhanes %>%
   mutate(gender = factor(gender, levels=c(1,2), labels=c("male", "female")),
-         ethnicity = factor(ethnicity, 
+         ethnicity = factor(ethnicity,
                              levels=c(1,2,3,4,6,7),
                              labels=c("mexican_american",
                                       "other_hispanic",
@@ -156,7 +72,7 @@ nhanes <-
                                       "non_hispanic_black",
                                       "non_hispanic_asian",
                                       "other_incl_multiracial")),
-         education = factor(education, 
+         education = factor(education,
                             levels=c(1,2,3,4,5,7,9),
                             labels=c("less_than_9th_grade",
                                      "9-11th_grade_no_diploma",
