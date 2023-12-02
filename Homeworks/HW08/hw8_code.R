6a5281fcb115dbe2f10cd901cd862f21051bdbc2
Flemming Wu <flemming.wu@gmail.com>
2023-12-02 14:13:50 -0800

    fix mistakes + add notes in HW 8

--- Homeworks/HW08/hw8_code.R
+++ Homeworks/HW08/hw8_code.R
@@ -19,24 +19,24 @@ depres.m <- glm(depres ~ dur.f, family = binomial, data = green)
 
 tibble(
     duration = as.numeric(levels(green$dur.f)), 
-    coef = depres.m$coefficients
+    coef = c(0, depres.m$coefficients[2:length(depres.m$coefficients)]) # the pred logit should start at 0 for the reference group
 ) %>%
   ggplot(aes(x=duration, y=coef)) +
   geom_point() +
   geom_line() +
-  geom_smooth(method = "glm", se = F, formula = "y ~ x") +
+  geom_smooth(method = "glm", se = F, formula = "y ~ x", color="red") +
   ggtitle("Depression Grouped Smooth")
 
 hibp.m <- glm(hibp ~ dur.f, family = binomial, data = green)
 
 tibble(
   duration = as.numeric(levels(green$dur.f)),
-  coef = hibp.m$coefficients
+  coef = c(0, hibp.m$coefficients[2:length(hibp.m$coefficients)])
 ) %>%
   ggplot(aes(x=duration, y=coef)) +
   geom_point() +
   geom_line() +
-  geom_smooth(method = "glm", se = F, formula = "y ~ x") +
+  geom_smooth(method = "glm", se = F, formula = "y ~ x", color = "red") +
   ggtitle("High Blood Pressure Grouped Smooth")
 
 # 1b
@@ -51,3 +51,6 @@ mfp(hibp ~ fp(dur), data = green, family = binomial)
 glm(depres ~ dur, data = green, family = binomial) %>% summary()
 green$dur_log <- log(((green$dur+7.5)/100)) # transformation suggested by `mfp`
 glm(hibp ~ dur_log, data = green, family = binomial) %>% summary()
+
+# logistic regression with duration category on high bp
+glm(hibp ~ factor(dur), data = green, family = binomial) |> summary()
